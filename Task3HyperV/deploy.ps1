6cea806957d769c32d3549c2c62aa8802ac63a39
dennistikhomirov <dennistikhomirov@gmail.com>
2019-07-06 02:25:38 -0700

    correct docx

--- Task3HyperV/deploy.ps1
+++ Task3HyperV/deploy.ps1
@@ -1,10 +1,10 @@
 [xml]$XmlDoc = Get-Content VMs.xml
-foreach ($VM in $XmlDoc.VMs.VM){
-if ($VM.isImport) {
-Import-VM -Path $VM.SourcePath -VhdDestinationPath $VM.VHDPath -Copy -GenerateNewId
-}
-  else{
-	.\createVM.ps1 $VM.Name $VM.SourcePath $VM.Memory $VM.VHDPath 
 
-}
+foreach ($VM in $XmlDoc.VMs.VM) {
+    if ($VM.isImport) {
+    Import-VM -Path $VM.SourcePath -VhdDestinationPath $VM.VHDPath -Copy -GenerateNewId
+    }
+    else{
+	.\createVM.ps1 $VM.Name $VM.SourcePath $VM.Memory $VM.VHDPath
+    }
 }
--- Task3HyperV/removeAll.ps1
+++ Task3HyperV/removeAll.ps1
@@ -1,7 +1,7 @@
 [xml]$XmlDoc = Get-Content VMs.xml
-foreach ($VM in $XmlDoc.VMs.VM){
-remove-vm $VM.Name -force
-if (-Not($VM.isImport)){
-remove-item "$(Join-Path $VM.VHDPath $VM.Name).vhdx"
-}
+foreach ($VM in $XmlDoc.VMs.VM) {
+    remove-vm $VM.Name -force
+    if (-Not($VM.isImport)) {
+        remove-item "$(Join-Path $VM.VHDPath $VM.Name).vhdx"
+    }
 }
